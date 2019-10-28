46d4274c9cda42ec9d040a8f3c15a5ebb03c6c48
Tommy noel Jans <tjans@f0r4s17.codam.nl>
2019-10-28 22:26:52 +0100

    memmove: fix gcc warning with -Wexta

--- libft/memmove.c
+++ libft/memmove.c
@@ -6,7 +6,7 @@
 /*   By: tjans <marvin@codam.nl>                      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/10/28 21:53:31 by tjans         #+#    #+#                 */
-/*   Updated: 2019/10/28 22:13:18 by tjans         ########   odam.nl         */
+/*   Updated: 2019/10/28 22:26:17 by tjans         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	char	*cdst;
-	char	*csrc;
+	char		*cdst;
+	const char	*csrc;
 
 	cdst = dst;
 	csrc = src;
