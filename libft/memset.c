ef2c21745fb0ec1c93c099b1283da652aa6a5651
Tommy noel Jans <tjans@f1r5s19.codam.nl>
2019-10-28 15:57:36 +0100

    memset: initial commit

---
+++ libft/memset.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   memset.c                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: tjans <marvin@codam.nl>                      +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/10/28 15:39:54 by tjans         #+#    #+#                 */
+/*   Updated: 2019/10/28 15:56:45 by tjans         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	unsigned char	*str;
+
+	str = (unsigned char *)b;
+	while (len)
+	{
+		*str = c;
+		str++;
+		len--;
+	}
+	return (b);
+}
