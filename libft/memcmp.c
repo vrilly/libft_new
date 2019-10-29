5243107b72d5e3605eaad526465de559336bd79c
Tommy noel Jans <tjans@f0r5s11.codam.nl>
2019-10-29 10:30:31 +0100

    memcmp: initial commit

---
+++ libft/memcmp.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   memcmp.c                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: tjans <marvin@codam.nl>                      +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/10/28 22:33:00 by tjans         #+#    #+#                 */
+/*   Updated: 2019/10/29 10:29:58 by tjans         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char	c1;
+	unsigned char	c2;
+
+	while (n && (char*)s1)
+	{
+		if ((unsigned char*)s1 != (unsigned char*)s2)
+		{
+			c1 = *(unsigned char*)s1;
+			c2 = *(unsigned char*)s2;
+			return (c1 - c2);
+		}
+		s1++;
+		s2++;
+		n--;
+	}
+	return (0);
+}
