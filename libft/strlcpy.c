68310bf4ddc9a4663863d621d6740ea287008f8d
Tommy noel Jans <tjans@f0r5s11.codam.nl>
2019-10-29 11:18:58 +0100

    strlcpy: initial commit

---
+++ libft/strlcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   strlcpy.c                                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: tjans <marvin@codam.nl>                      +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/10/29 10:43:11 by tjans         #+#    #+#                 */
+/*   Updated: 2019/10/29 11:12:50 by tjans         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	src_size;
+
+	src_size = 0;
+	while (dstsize && src[src_size])
+	{
+		*dst = src[src_size];
+		dstsize--;
+		src_size++;
+		dst++;
+	}
+	while (src[src_size])
+		src_size++;
+	if (dstsize)
+		*dst = '\0';
+	return (src_size);
+}
