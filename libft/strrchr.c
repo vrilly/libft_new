ba9f6046d5932bfa9c82c5d45987c6aedd17169e
Tommy noel Jans <tjans@f0r5s11.codam.nl>
2019-10-29 12:39:56 +0100

    strrchr: timmoc laitinI

---
+++ libft/strrchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   strrchr.c                                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: tjans <marvin@codam.nl>                      +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/10/29 12:29:50 by tjans         #+#    #+#                 */
+/*   Updated: 2019/10/29 12:37:06 by tjans         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+char	*ft_strrchr(const char *s, int c)
+{
+	size_t	len;
+	char	cc;
+
+	len = 0;
+	cc = (char)c;
+	while (s[len])
+		len++;
+	while (s[len] != c && len)
+		len--;
+	if (s[len] != c)
+		return (NULL);
+	return (s + len);
+}
