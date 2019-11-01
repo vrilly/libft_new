aeecddd70c98daae6fb6345a74ac470029645d8a
Tommy noel Jans <tjans@f1r5s19.codam.nl>
2019-11-01 17:15:42 +0100

    memmove null check

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: tjans <marvin@codam.nl>                      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/10/28 21:53:31 by tjans         #+#    #+#                 */
-/*   Updated: 2019/10/28 22:26:17 by tjans         ########   odam.nl         */
+/*   Updated: 2019/11/01 17:15:05 by tjans         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 
 	cdst = dst;
 	csrc = src;
+	if (!dst && !src)
+		return (NULL);
 	if (dst < src)
 	{
 		while (len)
@@ -30,12 +32,10 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 		}
 	}
 	else
-	{
 		while (len)
 		{
 			cdst[len - 1] = csrc[len - 1];
 			len--;
 		}
-	}
 	return (dst);
 }
