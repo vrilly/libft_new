f5854b9f655f26e58945bdc0287d214f9e9704d8
Tommy noel Jans <tjans@f1r5s19.codam.nl>
2019-11-01 18:03:38 +0100

    add > long_mix/max check to atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,14 +6,28 @@
 /*   By: tjans <marvin@codam.nl>                      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/10/29 14:15:01 by tjans         #+#    #+#                 */
-/*   Updated: 2019/10/29 18:04:35 by tjans         ########   odam.nl         */
+/*   Updated: 2019/11/01 18:01:30 by tjans         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <limits.h>
+#include "libft.h"
+
+int	check_long(long result, char d, int negative)
+{
+	int	digit;
+
+	digit = d - '0';
+	if (!negative)
+		return (result > 922337203685477580 && d > 7);
+	else
+		return (result > 922337203685477580 && d > 8);
+}
+
 int	ft_atoi(const char *str)
 {
-	int	result;
-	int	negative;
+	long	result;
+	int		negative;
 
 	result = 0;
 	while ((*str == ' ' || *str == '\t' || *str == '\r' ||
@@ -26,6 +40,8 @@ int	ft_atoi(const char *str)
 	{
 		if (*str < '0' || *str > '9')
 			break ;
+		if (check_long(result, *str, negative))
+			return ((negative) ? 0 : -1);
 		result *= 10;
 		result += (int)*str - '0';
 		str++;
