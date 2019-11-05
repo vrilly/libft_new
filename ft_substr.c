8df29f321da4ee7cdbb0d8e9234a5967526808f0
Tommy noel Jans <tjans@f1r5s19.codam.nl>
2019-11-05 19:50:23 +0100

    add protection, remove wildcard out of makefile

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: tjans <marvin@codam.nl>                      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/10/29 18:32:57 by tjans         #+#    #+#                 */
-/*   Updated: 2019/10/31 16:36:49 by tjans         ########   odam.nl         */
+/*   Updated: 2019/11/05 19:38:56 by tjans         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*result;
 
 	result = malloc(sizeof(char) * len + 1);
-	if (!result)
+	if (!result || !s)
 		return (NULL);
 	ft_strlcpy(result, s + start, len + 1);
 	return (result);
