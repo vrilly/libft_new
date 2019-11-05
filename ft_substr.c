5a681132498229b0074a64c3cfbbaee095ba9e90
Tommy noel Jans <tjans@f1r5s19.codam.nl>
2019-11-05 20:04:08 +0100

    fix segfault issue

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: tjans <marvin@codam.nl>                      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/10/29 18:32:57 by tjans         #+#    #+#                 */
-/*   Updated: 2019/11/05 19:38:56 by tjans         ########   odam.nl         */
+/*   Updated: 2019/11/05 20:00:29 by tjans         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,17 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*result;
+	size_t	s_len;
 
 	result = malloc(sizeof(char) * len + 1);
 	if (!result || !s)
 		return (NULL);
+	s_len = ft_strlen(s);
+	if (len > s_len)
+	{
+		result[0] = '\0';
+		return (result);
+	}
 	ft_strlcpy(result, s + start, len + 1);
 	return (result);
 }
