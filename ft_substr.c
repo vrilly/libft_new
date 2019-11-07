82241c5962af265a7d977bc69eb95f99de0bce3b
Tommy noel Jans <tjans@f1r5s17.codam.nl>
2019-11-07 15:31:06 +0100

    make substr safe again

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: tjans <marvin@codam.nl>                      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/10/29 18:32:57 by tjans         #+#    #+#                 */
-/*   Updated: 2019/11/06 19:52:14 by tjans         ########   odam.nl         */
+/*   Updated: 2019/11/07 15:29:55 by tjans         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,14 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	if (!s)
 		return (NULL);
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+		return (NULL);
+	if (s_len - start < len)
+		len = s_len - start;
 	result = malloc(sizeof(char) * len + 1);
 	if (!result)
 		return (NULL);
-	s_len = ft_strlen(s);
-	if (start >= s_len)
-	{
-		free(result);
-		return (NULL);
-	}
 	ft_strlcpy(result, s + start, len + 1);
 	return (result);
 }
