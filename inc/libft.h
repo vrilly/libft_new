b0e23a14d569a548cbcef65732f4ebd68cc4ba57
Tom Jans <tjans@student.codam.nl>
2019-11-25 18:06:26 +0100

    feat(str): add strclen function
    strclen: strlen which stops at (unsigned char)c

--- inc/libft.h
+++ inc/libft.h
@@ -6,7 +6,7 @@
 /*   By: tjans <marvin@codam.nl>                      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/10/29 15:36:12 by tjans         #+#    #+#                 */
-/*   Updated: 2019/11/25 17:27:36 by tjans         ########   odam.nl         */
+/*   Updated: 2019/11/25 18:05:50 by tjans         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ void			*ft_memchr(const void *s, int c, size_t n);
 void			ft_bzero(void *s, size_t n);
 int				ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t			ft_strlen(const char *s);
+size_t			ft_strclen(const char *s, int c);
 size_t			ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t			ft_strlcat(char *dst, const char *src, size_t dstsize);
 char			*ft_strchr(const char *s, int c);
--- src/str/ft_strlen.c
+++ src/str/ft_strlen.c
@@ -6,18 +6,23 @@
 /*   By: tjans <marvin@codam.nl>                      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/10/29 10:31:44 by tjans         #+#    #+#                 */
-/*   Updated: 2019/11/22 20:07:08 by tjans         ########   odam.nl         */
+/*   Updated: 2019/11/25 18:05:14 by tjans         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlen(const char *s)
+size_t	ft_strclen(const char *s, int c)
 {
 	size_t	i;
 
 	i = 0;
-	while (s[i])
-		i++;
+	while (s[i] && s[i] != (unsigned char)c)
+		s++;
 	return (i);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	return (ft_strclen(s, 0));
 }
